ab5ac08be48627728627447206ca517ca46bac77
elmin412 <115942958+elmin412@users.noreply.github.com>
2023-12-30 22:15:01 +0100

    first-push

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,25 @@
 `use strict`;
 
-function slugify(title) {
-  const titleLow = title.toLowerCase();
+function isEnoughCapacity(products, containerSize) {
+  let totalProduct = 0;
 
-  const titleSplit = titleLow.split(" ");
+  const productsValue = Object.values(products);
 
-  return titleSplit.join("-");
+  for (let product of productsValue) {
+    totalProduct += product;
+  }
+
+  if (totalProduct <= containerSize) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // fals
